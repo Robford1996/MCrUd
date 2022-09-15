8a366ed650cca45efdcbc3002278a7962ebe7743
Robert Ford <robert.ford1996@gmail.com>
2022-09-14 21:52:05 -0400

    first commit

---
+++ models/controllers/heros.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router()
+const Heros = require("../models/heros.js")
+
+//INDEX
+
+//NEW
+
+//DELETE
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
+
+module.exports = router
